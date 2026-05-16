f89b93a4939690dfaf4301ed5d5cfe1b38f6a1eb
Naol-Fikadu <fikaduhundaol9@gmail.com>
2026-05-16 11:01:02 +0300

    Add external view function to return sum

--- Codebases/Arguments.sol
+++ Codebases/Arguments.sol
@@ -1,1 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+contract Contract {
+    uint public x;
+    
+    constructor(uint _x) {
+        x = _x;
+    }
+    
+    function increment() external {
+        x = x + 1;
+    }
+    
+    // NEW: External view function that returns sum
+    function add(uint _value) external view returns(uint) {
+        return x + _value;
+    }
+}
