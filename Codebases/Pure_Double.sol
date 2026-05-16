f4b2c46427337da367375d53c886b377e77cb0aa
Naol-Fikadu <fikaduhundaol9@gmail.com>
2026-05-16 11:08:25 +0300

    Add basic contract with increment and add functions

--- Codebases/Pure_Double.sol
+++ Codebases/Pure_Double.sol
@@ -1,1 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "forge-std/console.sol";
+
+contract Contract {
+    uint public x;
+    
+    constructor() {
+        x = 0;
+    }
+    
+    function increment() external {
+        x++;
+    }
+    
+    function add(uint _value) external view returns(uint) {
+        return x + _value;
+    }
+    
+    function winningNumber(string calldata _message) external pure returns (uint) {
+        return 794;
+    }
+}
